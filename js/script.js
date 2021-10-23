ea3dfd63a26adf1d52050cc521a54db9b188bddf
Olena <pampam32121@gmail.com>
2021-10-24 01:59:36 +0300

    refactored code using fetch API instead of XMLHttpRequest

--- js/script.js
+++ js/script.js
@@ -241,27 +241,28 @@ window.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json');
-
             const formData = new FormData(form);
             const object = {};
             formData.forEach(function(value, key) {
                 object[key] = value;
             });
-            const json = JSON.stringify(object);
-            request.send(json);
 
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then((data) => data.text())
+            .then((data) => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
             });
             
         });
@@ -290,21 +291,5 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
-
-
-    //My variant of modal
-    /* const modalTrigger = document.querySelectorAll('[data-modal]'),
-        modalClose = document.querySelector('[data-close]'),
-        modalWindow = document.querySelector('.modal');
-
-    modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-            modalWindow.style.display = 'block';
-        });
-    });
-
-    modalClose.addEventListener('click', () => {
-        modalWindow.style.display = 'none';
-    }); */
     
 });
